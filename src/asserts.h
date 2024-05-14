1b9b0227b27d2cb08e63ba0405006a6be6932297
Žan Sovič <soviczan7@gmail.com>
2024-05-15 00:28:12 +0200

    added asserts: assert_eq, assert_neq, panic, todo, unimplemented

---
+++ src/asserts.h
@@ -0,0 +1,15 @@
+#ifndef ASSERTS_H
+#define ASSERTS_H
+
+#define panic(...) fprintf(stderr, __VA_ARGS__); exit(EXIT_FAILURE)
+
+#define todo() fprintf(stderr, "%s not yet implemented", __func__); exit(EXIT_FAILURE)
+#define todo_msg(...) fprintf(stderr, "%s not yet implemented: ", __func__); fprintf(stderr, __VA_ARGS__); exit(EXIT_FAILURE)
+
+#define unimplemented() fprintf(stderr, "%s not implemented", __func__); exit(EXIT_FAILURE)
+#define unimplemented_msg(...) fprintf(stderr, "%s not implemented: ", __func__); fprintf(stderr, __VA_ARGS__); exit(EXIT_FAILURE)
+
+#define assert_eq(lhs, rhs) assert(lhs == rhs)
+#define assert_neq(lhs, rhs) assert(lhs != rhs)
+
+#endif // ASSERTS_H
