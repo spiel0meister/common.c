e214dda8a2a4b1f37b67850468d97b800f273e29
Žan Sovič <soviczan7@gmail.com>
2024-05-18 23:55:00 +0200

    added da_remove

--- src/da.h
+++ src/da.h
@@ -3,10 +3,16 @@
 
 #define da_append(da, item) \
     if ((da)->size == (da)->capacity) { \
-        (da)->capacity = (da)->capacity == 0 ? 2 : (da)->capacity * 2; \
-        (da)->items = realloc((da)->items, sizeof((da)->items[0]) * (da)->capacity); \
+        (da)->capacity = (da)->capacity == 0? 2 : (da)->capacity * 2; \
+        (da)->items = realloc((da)->items, (da)->capacity * sizeof((da)->items[0])); \
+        assert((da)->items != NULL); \
     } \
     (da)->items[(da)->size++] = (item)
+
+#define da_remove(da, i, n) \
+    assert((i) + (n) <= (da)->size); \
+    memmove((da)->items + (i), (da)->items + (i) + 1, n * sizeof((da)->items[0]))
+
 
 #define da_foreach(da, type, ptr) for (type* ptr = (da)->items; ptr != (da)->items + (da)->size; ptr++)
 
