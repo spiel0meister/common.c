253ae6437f487fb90ba13b97ed695d5a98f6ee8e
Žan Sovič <soviczan7@gmail.com>
2024-05-19 15:04:03 +0200

    added variadic push_str function

--- src/string_builder.h
+++ src/string_builder.h
@@ -15,7 +15,10 @@ typedef struct {
 
 StringBuilder sbuilder_init(size_t init_capacity);
 void sbuilder_resize(StringBuilder* sbuilder);
+
 void sbuilder_push(StringBuilder* sbuilder, char c);
+void sbuilder_push_str_(StringBuilder* sbuilder, ...);
+#define sbuilder_push_str(sb, ...) sbuilder_push_str_(sb, __VA_ARGS__, NULL)
 
 #ifndef SBUILDER_MALLOC
     #define SBUILDER_MALLOC(n) malloc(sizeof(char) * (n))
@@ -23,10 +26,11 @@ void sbuilder_push(StringBuilder* sbuilder, char c);
 
 HeapString sbuilder_export(StringBuilder const* sbuilder);
 
-#ifdef STRING_BUILDER_IMPLENTATION
+#ifdef SBUILDER_IMPLEMENETATION
+#include <assert.h>
+#include <stdarg.h>
 #include <stdlib.h>
 #include <string.h>
-#include <assert.h>
 
 void sbuilder_resize(StringBuilder* sbuilder) {
     sbuilder->capacity = sbuilder->capacity == 0 ? 2 : sbuilder->capacity * 2;
@@ -41,17 +45,26 @@ void sbuilder_push(StringBuilder* sbuilder, char c) {
     sbuilder->items[sbuilder->size++] = c;
 }
 
-void sbuilder_push_str(StringBuilder* sbuilder, char* cstr) {
-    size_t len = strlen(cstr);
-    while (sbuilder->size + len >= sbuilder->capacity) {
-        sbuilder_resize(sbuilder);
+void sbuilder_push_str_(StringBuilder* sbuilder, ...) {
+    va_list list;
+    va_start(list, sbuilder);
+
+    char* cstr = va_arg(list, char*);
+    while (cstr != NULL) {
+        size_t len = strlen(cstr);
+        while (sbuilder->size + len >= sbuilder->capacity) {
+            sbuilder_resize(sbuilder);
+        }
+        memcpy(sbuilder->items + sbuilder->size, cstr, len);
+        sbuilder->size += len;
+        cstr = va_arg(list, char*);
     }
-    memcpy(sbuilder->items + sbuilder->size, cstr, len);
-    sbuilder->size += len;
+
+    va_end(list);
 }
 
 HeapString sbuilder_export(StringBuilder const* sbuilder) {
-    char* heap_mem = STRING_MALLOC(sbuilder->size + 1);
+    char* heap_mem = SBUILDER_MALLOC(sbuilder->size + 1);
     memcpy(heap_mem, sbuilder->items, sbuilder->size);
     heap_mem[sbuilder->size] = 0;
     return (HeapString) {
