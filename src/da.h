d63566ffd81607939dca93227431c07985c47ede
Žan Sovič <soviczan7@gmail.com>
2024-05-26 14:52:44 +0200

    added da_append_many, da_get, da_set, da_first, da_last, DA_INIT_CAP

--- src/da.h
+++ src/da.h
@@ -1,13 +1,36 @@
 #ifndef DA_H
 #define DA_H
 
+#define DA_INIT_CAP 256
+
 #define da_append(da, item) \
     if ((da)->size == (da)->capacity) { \
-        (da)->capacity = (da)->capacity == 0? 2 : (da)->capacity * 2; \
+        (da)->capacity = (da)->capacity == 0? DA_INIT_CAP : (da)->capacity * 2; \
         (da)->items = realloc((da)->items, (da)->capacity * sizeof((da)->items[0])); \
         assert((da)->items != NULL); \
     } \
     (da)->items[(da)->size++] = (item)
+
+#define da_append_many(da, items, items_size) do { \
+        if ((da)->size + (items_size) > (da)->capacity) {\
+            if ((da)->capacity == 0) {\
+                (da)->capacity = DA_INIT_CAP; \
+            } \
+            while ((da)->size + (items_size) > (da)->capacity) {\
+                (da)->capacity = (da)->capacity * 2; \
+            } \
+            (da)->items = realloc((da)->items, (da)->capacity * sizeof((da)->items[0])); \
+            assert((da)->items != NULL); \
+        } \
+        memcpy((da)->items + (da)->size, items, (items_size) * sizeof(*((da)->items))); \
+        (da)->size += items_size; \
+    } while (0)
+
+#define da_get(da, i) (da)->items[i]
+#define da_set(da, i, value) (da)->items[i] = value
+
+#define da_first(da) (da)->items[0]
+#define da_last(da) (da)->items[(da)->size - 1]
 
 #define da_remove(da, i, n) \
     assert((i) + (n) <= (da)->size); \
