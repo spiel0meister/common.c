5db34804b568be59428357947f659d00e731c703
Žan Sovič <soviczan7@gmail.com>
2024-05-29 22:44:16 +0200

    feat(log.h): added the option to set log file

--- src/log.h
+++ src/log.h
@@ -1,5 +1,6 @@
 #ifndef LOG_H
 #define LOG_H
+#include <stdio.h>
 
 typedef enum {
     LOG_INFO = 0,
@@ -16,35 +17,52 @@ typedef enum {
 __attribute__((format(printf, 2, 3)))
 void log_log(LogLevel level, const char* fmt, ...);
 
+void log_set_file(FILE* f);
+
 void log_set_level(LogLevel level);
 
 #endif // LOG_H
 
 #ifdef LOG_IMPLEMENTATION
 #include <stdarg.h>
-#include <stdio.h>
 
+typedef struct {
+    LogLevel level;
+    FILE* file;
+}Logger;
+
+static_assert(LOG_COUNT_ == 3, "LogLevel enum grew");
 static char* LEVEL_TO_STR[] = {
     [LOG_ERROR] = "ERROR",
     [LOG_WARN] = "WARN",
     [LOG_INFO] = "INFO",
 };
-static_assert(LOG_COUNT_ == 3, "LogLevel enum grew");
 
-static LogLevel log_level = LOG_INFO;
+static Logger logger = { LOG_INFO, NULL };
 void log_log(LogLevel level, const char* fmt, ...) {
+    if (logger.file == NULL) {
+        printf("[%s] ", LEVEL_TO_STR[LOG_INFO]);
+        printf("log file not set, default to stdout");
+        printf("\n");
+        logger.file = stdout;
+    }
+
     va_list args;
     va_start(args, fmt);
-    if (level >= log_level) {
-        printf("[%s] ", LEVEL_TO_STR[level]);
-        vprintf(fmt, args);
-        printf("\n");
+    if (level >= logger.level) {
+        fprintf(logger.file,"[%s] ", LEVEL_TO_STR[level]);
+        vfprintf(logger.file, fmt, args);
+        fprintf(logger.file, "\n");
     }
     va_end(args);
 }
 
+void log_set_file(FILE* f) {
+    logger.file = f;
+}
+
 void log_set_level(LogLevel level) {
-    log_level = level;
+    logger.level = level;
 }
 #endif
 
