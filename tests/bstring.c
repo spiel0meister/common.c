1369a5f018485ee8fa2dacc3e47b506108581300
Žan Sovič <soviczan7@gmail.com>
2024-06-03 23:46:51 +0200

    added additional test

--- tests/bstring.c
+++ tests/bstring.c
@@ -1,7 +1,38 @@
 #define TEST_IMPLEMENTATION
 #include "../ctest.h"
 
+#define BSTRING_IMPLEMENTATION
+#include "../src/bstring.h"
+
+Test(shortf_test) {
+    ShortString a = shortf("%d %d", 1, 2);
+    test_assert_str_eq(a.buf, "1 2");
+
+    a = shortf("%s %d", a.buf, 3);
+    test_assert_str_eq(a.buf, "1 2 3");
+
+    a = shortf("%s %.1f", a.buf, 2.5f);
+    test_assert_str_eq(a.buf, "1 2 3 2.5");
+
+    return TEST_SUCCES;
+}
+
+Test(str_start_endswith_test) {
+    test_assert(str_startswith("hello", "hell"));
+    test_assert(str_startswith("world", "wor"));
+    test_assert_not(str_startswith("google", "lol"));
+    
+
+    test_assert(str_endswith("a.txt", ".txt"));
+    test_assert(str_endswith("a.png", ".png"));
+    test_assert_not(str_endswith("google", "lol"));
+
+    return TEST_SUCCES;
+}
+
 int main(void) {
+    test_register(shortf_test);
+    test_register(str_start_endswith_test);
     test_run_all_sync(true);
     return 0;
 }
--- tests/string_builder.c
+++ tests/string_builder.c
@@ -14,8 +14,61 @@ Test(sbuilder_init_test) {
     return TEST_SUCCES;
 }
 
+Test(sbuilder_push_test) {
+    StringBuilder sb = sbuilder_init(10);
+    sbuilder_push_str(&sb, "hello");
+    test_assert(sb.items != NULL);
+    test_assert(sb.capacity == 10);
+    test_assert(sb.size == 5);
+
+    sbuilder_push_str(&sb, "h", "n");
+    test_assert(sb.items != NULL);
+    test_assert(sb.capacity == 10);
+    test_assert(sb.size == 7);
+    sbuilder_free(&sb);
+
+    return TEST_SUCCES;
+}
+
+Test(sbuilder_export_test) {
+    StringBuilder sb = sbuilder_init(10);
+    sbuilder_push_str(&sb, "hello");
+
+    char* out = sbuilder_export(&sb);
+    test_assert_str_eq(out, "hello");
+    free(out);
+    
+    sbuilder_push_str(&sb, " world");
+    out = sbuilder_export(&sb);
+
+    test_assert_str_eq(out, "hello world");
+    free(out);
+
+    return TEST_SUCCES;
+}
+
+Test(sbuilder_export_inplace_test) {
+    StringBuilder sb = sbuilder_init(10);
+    sbuilder_push_str(&sb, "hello");
+    
+    char out[16] = {};
+    sbuilder_export_inplace(&sb, out);
+    test_assert_str_eq(out, "hello");
+
+    sbuilder_push_str(&sb, " world");
+    sbuilder_export_inplace(&sb, out);
+    test_assert_str_eq(out, "hello world");
+
+    sbuilder_free(&sb);
+
+    return TEST_SUCCES;
+}
+
 int main(void) {
     test_register(sbuilder_init_test);
+    test_register(sbuilder_push_test);
+    test_register(sbuilder_export_test);
+    test_register(sbuilder_export_inplace_test);
 
     test_run_all_sync(true);
     return 0;
