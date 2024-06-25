ea6d4c570308c013d7157a89db88cc5d2fd0e517
spiel0meister <soviczan7@gmail.com>
2024-06-25 16:51:35 +0200

    adder sb_read_file

--- src/string_builder.h
+++ src/string_builder.h
@@ -1,6 +1,7 @@
 #ifndef STRING_BUILDER_H
 #define STRING_BUILDER_H
 #include <stddef.h>
+#include <stdbool.h>
 
 #ifndef SB_INIT_CAP
 #define SB_INIT_CAP 8
@@ -19,12 +20,14 @@ void sb_maybe_resize(StringBuilder* sb, size_t to_append_len);
 void sb_push(StringBuilder* sb, char c);
 
 void sb_push_str_null(StringBuilder* sb, ...);
-
 // Pushes a variadic amount of cstrings to a string builder 
 #define sb_push_str(sb, ...) sb_push_str_null(sb, __VA_ARGS__, NULL)
 
 // Pushes a sized string of certain length to a string builder 
 void sb_push_nstr(StringBuilder* self, const char* str, size_t len);
+
+// Reads the provided file and appends its contents into the provided SB.
+bool sb_read_file(StringBuilder* sb, const char* filepath); 
 
 // Exports the contents of a string builder to heap memory 
 char* sb_export(StringBuilder const* sb);
@@ -85,6 +88,23 @@ void sb_push_nstr(StringBuilder* sb, const char* str, size_t len) {
     memcpy(sb->items + sb->count, str, len);
     sb->count += len;
 }
+
+bool sb_read_file(StringBuilder* sb, const char* filepath) {
+    FILE* f = fopen(filepath, "rb");
+    if (f == NULL) return false;
+
+    fseek(f, 0, SEEK_END);
+    size_t n = ftell(f);
+    fseek(f, 0, SEEK_SET);
+
+    sb_maybe_resize(sb, n);
+    int n_ = fread(sb->items, 1, n, f);
+    assert(n_ == (int)n);
+    sb->count += n_;
+
+    fclose(f);
+    return true;
+} 
 
 char* sb_export(StringBuilder const* sb) {
     char* out = SB_MALLOC(sizeof(char) * (sb->count + 1));
