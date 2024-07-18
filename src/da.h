f60abd1fe7c818f137144c3bf773cc08f0ed5107
spiel0meister <soviczan7@gmail.com>
2024-07-19 01:13:16 +0200

    added da_with_cap, da_with_cap2

--- src/da.h
+++ src/da.h
@@ -1,7 +1,11 @@
 #ifndef DA_H_
 #define DA_H_
-#include <stddef.h>
 #include <stdint.h>
+
+#ifndef DA_SIZE_TYPE
+#include <stddef.h>
+#define DA_SIZE_TYPE size_t
+#endif // DA_INT_TYPE
 
 #ifndef DA_SORT
 #include <stdlib.h>
@@ -13,20 +17,28 @@
 #define DA_ASSERT(expr) assert(expr)
 #endif // DA_ASSERT
 
-#define DA(Type) Type* items; size_t count; size_t capacity
+#define DA(Type) Type* items; DA_SIZE_TYPE count; DA_SIZE_TYPE capacity
+
+#define da_with_cap(da, cap) \
+    (DA_ASSERT(sizeof((da)->capacity) == sizeof(DA_SIZE_TYPE)), __da_with_cap(da, (uint8_t*)&(da)->items - (uint8_t*)(da), (uint8_t*)&(da)->capacity - (uint8_t*)(da), cap, sizeof((da)->items[0])))
+
+#define da_with_cap2(da, cap) \
+    (DA_ASSERT(sizeof((da)->capacity) == sizeof(DA_SIZE_TYPE)), __da_with_cap2(da, (uint8_t*)&(da)->items - (uint8_t*)(da), (uint8_t*)&(da)->capacity - (uint8_t*)(da), cap, sizeof((da)->items[0])))
 
 #define da_append(da, item) \
-    (DA_ASSERT(sizeof((da)->count) == sizeof(size_t) && sizeof((da)->capacity) == sizeof(size_t)), __da_append(da, \
+    (DA_ASSERT(sizeof((da)->count) == sizeof(DA_SIZE_TYPE) && sizeof((da)->capacity) == sizeof(DA_SIZE_TYPE)), __da_append(da, \
             (uint8_t*)&(da)->items - (uint8_t*)(da), \
             (uint8_t*)&(da)->count - (uint8_t*)(da), \
             (uint8_t*)&(da)->capacity - (uint8_t*)(da), item, sizeof(*(item))))
 
 #define da_append_many(da, items_, item_count) \
-    (DA_ASSERT(sizeof((da)->count) == sizeof(size_t) && sizeof((da)->capacity) == sizeof(size_t)), __da_append_many(da, \
+    (DA_ASSERT(sizeof((da)->count) == sizeof(DA_SIZE_TYPE) && sizeof((da)->capacity) == sizeof(DA_SIZE_TYPE)), __da_append_many(da, \
             (uint8_t*)&(da)->items - (uint8_t*)(da), \
             (uint8_t*)&(da)->count - (uint8_t*)(da), \
             (uint8_t*)&(da)->capacity - (uint8_t*)(da), items_, item_count, sizeof(items_[0])))
 
+void __da_with_cap(void* da, size_t items_field_offset, size_t capacity_field_offset, DA_SIZE_TYPE init_cap, size_t item_size);
+void __da_with_cap2(void* da, size_t items_field_offset, size_t capacity_field_offset, DA_SIZE_TYPE init_cap, size_t item_size);
 void __da_append(void* da, size_t items_field_offset, size_t count_field_offset, size_t capacity_field_offset, void* item, size_t item_size);
 void __da_append_many(void* da, size_t items_field_offset, size_t count_field_offset, size_t capacity_field_offset, void* items, size_t item_count, size_t item_size);
 
@@ -56,13 +68,31 @@ void __da_append_many(void* da, size_t items_field_offset, size_t count_field_of
     #define DA_INIT_CAP 64
 #endif // DA_INIT_CAP
 
+void __da_with_cap(void* da, size_t items_field_offset, size_t capacity_field_offset, DA_SIZE_TYPE init_cap, size_t item_size) {
+    void** items = (da + items_field_offset);
+    DA_SIZE_TYPE* capacity = (DA_SIZE_TYPE*)(da + capacity_field_offset);
+
+    *capacity = init_cap;
+    *items = malloc(*capacity * item_size);
+    assert(*items != NULL);
+}
+
+void __da_with_cap2(void* da, size_t items_field_offset, size_t capacity_field_offset, DA_SIZE_TYPE init_cap, size_t item_size) {
+    void** items = (da + items_field_offset);
+    DA_SIZE_TYPE* capacity = (DA_SIZE_TYPE*)(da + capacity_field_offset);
+
+    DA_SIZE_TYPE real_cap = 1;
+    while (real_cap < init_cap) real_cap *= 2;
+
+    *capacity = real_cap;
+    *items = malloc((*capacity) * item_size);
+    assert(*items != NULL);
+}
+
 void __da_append(void* da, size_t items_field_offset, size_t count_field_offset, size_t capacity_field_offset, void* item, size_t item_size) {
     void** items = (void**)(da + items_field_offset);
-    size_t* count = (size_t *)(da + count_field_offset);
-    size_t* capacity = (size_t *)(da + capacity_field_offset);
-
-    // printf("count: %ld, capacity: %ld\n", *count, *capacity);
-    // printf("items: %p\n", *items);
+    DA_SIZE_TYPE* count = (DA_SIZE_TYPE *)(da + count_field_offset);
+    DA_SIZE_TYPE* capacity = (DA_SIZE_TYPE *)(da + capacity_field_offset);
 
     if (*count >= *capacity) { 
         if (*capacity == 0) *capacity = DA_INIT_CAP; 
@@ -77,8 +107,8 @@ void __da_append(void* da, size_t items_field_offset, size_t count_field_offset,
 
 void __da_append_many(void* da, size_t items_field_offset, size_t count_field_offset, size_t capacity_field_offset, void* items, size_t item_count, size_t item_size) {
     void** p_items = (void**)(da + items_field_offset);
-    size_t* count = (size_t *)(da + count_field_offset);
-    size_t* capacity = (size_t *)(da + capacity_field_offset);
+    DA_SIZE_TYPE* count = (DA_SIZE_TYPE *)(da + count_field_offset);
+    DA_SIZE_TYPE* capacity = (DA_SIZE_TYPE *)(da + capacity_field_offset);
 
     if (*count + item_count >= *capacity) { 
         if (*capacity == 0) *capacity = DA_INIT_CAP; 
