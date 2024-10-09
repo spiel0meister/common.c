085e824536bf595967eba8a40e32e713a3738b75
spiel0meister <soviczan7@gmail.com>
2024-10-09 17:37:03 +0200

    added stuff to dah.h

--- src/dah.h
+++ src/dah.h
@@ -6,10 +6,12 @@
 #include <stdint.h>
 
 #ifndef DAH_MALLOC
+#include <stdlib.h>
 #define DAH_MALLOC malloc
 #endif // DAH_MALLOC
 
 #ifndef DAH_REALLOC
+#include <stdlib.h>
 #define DAH_REALLOC realloc
 #endif // DAH_REALLOC
 
@@ -20,27 +22,28 @@ typedef struct {
 
 #define DAH_INIT_CAPACITY 16
 
-#define dah_append(da, v) ((da) = dah__maybe_resize(da, 1, sizeof(*(da))), (da)[dah__getheader(da)->count++] = (v))
-#define dah_append_many(da, vs, vcount) ((da) = dah__maybe_resize(da, vcount, sizeof(*(da))), memcpy((da) + dah__getheader(da)->count, vs, vcount * sizeof(*(da))), dah__getheader(da)->count += vcount)
-#define dah_remove_unordered(da, i) ((da)[i] = (da)[--dah__getheader(da)->count])
+#define dah_append(da, v) ((da) = dah__maybe_resize(da, 1, sizeof(*(da))), (da)[dah_getheader(da)->count++] = (v))
+#define dah_append_many(da, vs, vcount) ((da) = dah__maybe_resize(da, vcount, sizeof(*(da))), memcpy((da) + dah_getheader(da)->count, vs, vcount * sizeof(*(da))), dah_getheader(da)->count += vcount)
+#define dah_remove_unordered(da, i) ((da)[i] = (da)[--dah_getheader(da)->count])
 #define dah_remove_ordered(da, i) dah__remove_ordered(da, i, sizeof(*(da)))
+#define dah_reset(da) (da_getheader(da)->count = 0)
+#define dah_free(da) ((da) = dah__free(da))
 
-#define da_free(da) ((da) = dah__free(da))
+#define dah_for(da, counter) for (size_t counter = 0; counter < dah_getheader(da)->count; ++counter)
+#define dah_foreach(da, Type, ptr) for (Type* ptr = da; ptr < da + dah_getheader(da)->count; ++ptr)
 
-size_t dah_getlen(void* da);
-void dah_setlen(void* da, size_t count);
+DaHeader* dah_getheader(void* da);
 
-DaHeader* dah__default_header(size_t item_size);
 DaHeader* dah__default_header(size_t item_size);
 void* dah__maybe_resize(void* da, size_t to_add, size_t item_size);
-DaHeader* dah__getheader(void* da);
 void dah__remove_ordered(void* da, size_t i, size_t item_size);
 void* dah__free(void* da);
 
 #ifdef DAH_IMPLEMENTATION
+#include <string.h>
 
 DaHeader* dah__default_header(size_t item_size) {
-    return DAH_MALLOC(sizeof(DaHeader) + item_size * DA_INIT_CAPACITY);
+    return DAH_MALLOC(sizeof(DaHeader) + item_size * DAH_INIT_CAPACITY);
 }
 
 void* dah__maybe_resize(void* da, size_t to_add, size_t item_size) {
@@ -59,27 +62,19 @@ void* dah__maybe_resize(void* da, size_t to_add, size_t item_size) {
     return header->data;
 }
 
-DaHeader* dah__getheader(void* da) {
+DaHeader* dah_getheader(void* da) {
     return (DaHeader*)da - 1;
 }
 
 void dah__remove_ordered(void* da, size_t i, size_t item_size) {
-    DaHeader* header = dah__getheader(da);
+    DaHeader* header = dah_getheader(da);
 
     memmove(header->data + i * item_size, header->data + (i + 1) * item_size, (header->count - i - 1) * item_size);
     header->count--;
 }
 
-size_t dah_getlen(void* da) {
-    return dah__getheader(da)->count;
-}
-
-void dah_setlen(void* da, size_t count) {
-    dah__getheader(da)->count = count;
-}
-
 void* dah__free(void* da) {
-    free(dah__getheader(da));
+    free(dah_getheader(da));
     return NULL;
 }
 
