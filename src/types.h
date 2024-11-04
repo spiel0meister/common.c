4395fc15549b7db8f96370d15cf475e101eae82b
spiel0meister <soviczan7@gmail.com>
2024-11-04 23:26:51 +0100

    changes

--- src/types.h
+++ src/types.h
@@ -1,11 +1,13 @@
 #ifndef TYPES_H_
 #define TYPES_H_
+#include <stdio.h>
 #include <stdint.h>
 
 typedef uint8_t u8;
 typedef uint16_t u16;
 typedef uint32_t u32;
 typedef uint64_t u64;
+typedef unsigned __int128 u128;
 
 typedef int8_t i8;
 typedef int16_t i16;
@@ -13,7 +15,13 @@ typedef int32_t i32;
 typedef int64_t i64;
 typedef __int128 i128;
 
+typedef float f32;
+typedef double f64;
+
 typedef char* cstr;
+
+typedef FILE* Fp;
+typedef void* rawp;
 
 #endif // TYPES_H_
 
