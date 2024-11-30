b7e34125ee26d158c6e512fdfd4ed275fec32378
spiel0meister <soviczan7@gmail.com>
2024-11-30 16:29:28 +0100

    [fs.h] directly use linux stuff

--- src/fs.h
+++ src/fs.h
@@ -1,10 +1,11 @@
 #ifndef FS_H_
 #define FS_H_
+#include <stdbool.h>
+
+char* read_entire_file(const char* path);
+bool write_to_file(const char* path, const char* content, size_t n);
+
 #endif // FS_H_
-
-#include <stddef.h>
-
-char* read_file(const char* path, size_t* size);
 
 #ifdef FS_IMPLEMENTATION
 #undef FS_IMPLEMENTATION
@@ -14,24 +15,68 @@ char* read_file(const char* path, size_t* size);
 #include <string.h>
 #include <errno.h>
 
-char* read_file(const char* path, size_t* size) {
-    FILE* f = fopen(path, "rb");
-    if (f == NULL) {
+#ifdef _WIN32
+#error "TODO: Windows"
+#else
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#endif // _WIN32
+
+char* read_entire_file(const char* path) {
+    bool result = true;
+    char* content = NULL;
+
+    int fd = open(path, O_RDONLY);
+    if (fd < 0) {
         fprintf(stderr, "Couldn't open %s: %s\n", path, strerror(errno));
-        return NULL;
+        result = false;
+        goto defer;
     }
 
-    fseek(f, 0, SEEK_END);
-    size_t size_ = ftell(f);
-    fseek(f, 0, SEEK_SET);
+    struct stat file_stat;
+    if (fstat(fd, &file_stat) < 0) {
+        fprintf(stderr, "Couldn't stat %s: %s\n", path, strerror(errno));
+        result = false;
+        goto defer;
+    }
 
-    char* s = malloc(size_ + 1);
-    fread(s, 1, size_, f);
-    s[size_] = 0;
+    content = malloc(file_stat.st_size + 1);
+    if (read(fd, content, file_stat.st_size) < 0) {
+        fprintf(stderr, "Couldn't read %s: %s\n", path, strerror(errno));
+        result = false;
+        goto defer;
+    }
 
-    fclose(f);
-    if (size != NULL) *size = size_;
-    return s;
+defer:
+    if (!result) {
+        free(content);
+        content = NULL;
+    }
+    if (fd > 0) close(fd);
+
+    return content;
+}
+
+bool write_to_file(const char* path, const char* content, size_t n) {
+    bool result = true;
+
+    int fd = open(path, O_WRONLY | O_CREAT, 0644);
+    if (fd < 0) {
+        fprintf(stderr, "Couldn't open %s: %s\n", path, strerror(errno));
+        result = false;
+        goto defer;
+    }
+
+    if (write(fd, content, n) < 0) {
+        fprintf(stderr, "Couldn't write to %s: %s\n", path, strerror(errno));
+        result = false;
+        goto defer;
+    }
+
+defer:
+    if (fd > 0) close(fd);
+    return result;
 }
 
 #endif // FS_IMPLEMENTATION
